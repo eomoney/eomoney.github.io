7e8886e6ccef955bd659ad7e9605fdac2408d317
eomoney <40340328+eomoney@users.noreply.github.com>
2018-06-19 13:54:35 +0300

    girst_commit

---
+++ res/js/script.js
@@ -0,0 +1,13 @@
+function buttonclick() {
+  if (document.getElementById('a1').style.display == 'none') {
+    document.getElementById('a1').style.display = 'block';
+    document.getElementById('a2').style.display = 'block';
+    document.getElementById('more').style.display = 'none';
+    document.getElementById('smaller').style.display = 'block';
+  }else{
+    document.getElementById('a1').style.display = 'none';
+    document.getElementById('a2').style.display = 'none';
+    document.getElementById('more').style.display = 'block';
+    document.getElementById('smaller').style.display = 'none';
+  }
+}
